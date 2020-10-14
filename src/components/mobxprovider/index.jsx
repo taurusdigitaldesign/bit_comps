6936502f450613b09b9f521e5b09c0f02274bf33
taurusdigitaldesign <taurusdigitaldesign@gmail.com>
2020-10-14 20:21:44 +0800

    feat: mobx-provider change to jsx

--- src/components/mobxprovider/index.jsx
+++ src/components/mobxprovider/index.jsx
@@ -1,22 +1,14 @@
 /**
  * @name MobXProvider
- * @desc 
+ * @desc
  * mobx的hooks用法，提供了一个MobxProvider组件，与原来的Provider用法一致；
  * 调用全局数据的use方法：useStores(name)
  */
 import React from 'react';
 
-// create React Context
-type IValueMap = Record<string, any>;
+export const MobXProviderContext = React.createContext({});
 
-export const MobXProviderContext = React.createContext<IValueMap>({});
-
-// the component using to provide the global store
-export interface ProviderProps extends IValueMap {
-  children: React.ReactNode;
-}
-
-export function MobXProvider(props: ProviderProps) {
+export function MobXProvider(props) {
   const { children, ...stores } = props;
   const parentValue = React.useContext(MobXProviderContext);
   const mutableProviderRef = React.useRef({ ...parentValue, ...stores });
@@ -26,6 +18,6 @@ export function MobXProvider(props: ProviderProps) {
 }
 
 // a hook method using global store
-export function useStores(name: string) {
+export function useStores(name) {
   return React.useContext(MobXProviderContext)[name];
 }
